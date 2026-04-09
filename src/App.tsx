cff422a8038c5e27c4b3e9740bc4afa296b531cb
Adiul Alam Adil <adiulalam@gmail.com>
2026-04-09 02:01:49 +0100

    chore: add shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { TooltipProvider } from "@/components/ui/tooltip";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline text-blue-600">Tailwind is Working!</h1>;
+  return (
+    <TooltipProvider>
+      <h1 className="text-3xl font-bold underline text-blue-600">Tailwind is Working!</h1>
+    </TooltipProvider>
+  );
 };
 
 export default App;
