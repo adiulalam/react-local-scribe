2726a1dcfceb42ead955b27c45571692aafa8562
Adiul Alam Adil <adiulalam@gmail.com>
2026-04-09 02:11:47 +0100

    chore: prettier fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 const App = () => {
   return (
     <TooltipProvider>
-      <h1 className="text-3xl font-bold underline text-blue-600">Tailwind is Working!</h1>
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Tailwind is Working!
+      </h1>
     </TooltipProvider>
   );
 };
